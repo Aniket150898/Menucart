b4431dd003b87eb47ac31d1fa71c782d86ddfadf
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2022-05-22 21:17:54 +0530

    f commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,18 +12,19 @@ setShowLinks(!showLinks)
   return (
     <div className='navbar'>
       <div className='leftSide' id={showLinks ? 'open':'close'}><img src={logo} alt='logo' />
-      <div className='hiddenLinks'>
+      {/* <div className='hiddenLinks'>
       <Link to='/'>Home</Link>
           <Link to='/menu'>Menu</Link>
           <Link to='/about'>About</Link>
           <Link to='/contact'>Contact</Link>
-      </div></div>
+      </div> */}
+      </div>
       <div className='rightSide'>
           <Link to='/'>Home</Link>
           <Link to='/menu'>Menu</Link>
           <Link to='/about'>About</Link>
           <Link to='/contact'>Contact</Link>
-          <button onClick={click}><ReorderIcon /></button>
+          {/* <button onClick={click}><ReorderIcon /></button> */}
       </div> </div>
   )
 }
